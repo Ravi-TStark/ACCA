5b1f2b88fec7c5036de154d92dc126864dff1c10
Ravi-TStark <58636984+Ravi-TStark@users.noreply.github.com>
2020-09-15 19:20:54 +0530

    Update Que3.sh

--- Que3.sh
+++ Que3.sh
@@ -1,7 +1,13 @@
-cd 'Question2_Assn1'
+cd 'FOLDER_NAME'
 
+echo $"\n"
 for databaseName in $(ls .git/objects); do
-    echo $databaseName
+    STRING=$(ls .git/objects/$databaseName)
+    OUT=$(echo "$databaseName${STRING:0:4}")
+
+    if [ ${#OUT} -ge 6 ]; then echo $"Type: $(git cat-file -t $OUT) \n"
+    echo $"Content: \n$(git cat-file -p $OUT) \n\n"
+    fi
 done
 
 exit
